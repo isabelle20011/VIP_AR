af510f8a968655e3897c9479c414d11852eabc26
Jessie Tepper <jesstepper@lawn-128-61-50-163.lawn.gatech.edu>
2019-11-07 14:32:53 -0500

    scene button

---
+++ AR_VIP/Assets/Scripts/photoButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class photoButton : MonoBehaviour
+{
+    public void changeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
