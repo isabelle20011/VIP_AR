97a1ef65c1e9a772bdb6c33531b963c847525f47
Jessie Tepper <jesstepper@Jessies-MacBook-Pro.local>
2019-11-08 01:04:18 -0500

    another button

---
+++ AR_VIP/Assets/Scripts/mainButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainButton : MonoBehaviour
+{
+    public void changeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
